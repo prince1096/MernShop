832a722c2e0c444446615dbdb4944ae15d5c881d
prince1096 <princeraj10121996@gmail.com>
2024-11-11 13:17:17 +0530

    sending user details

--- backend/controller/user.Controller.js
+++ backend/controller/user.Controller.js
@@ -133,6 +133,13 @@ const userDetailsController = async (req, res) => {
 
     const user = await User.findById(req.userId);
     console.log("user", user);
+
+    res.status(200).json({
+      data: user,
+      error: false,
+      success: true,
+      message: "User Details",
+    });
   } catch (error) {
     res.status(400).json({
       message: error.message || error,
