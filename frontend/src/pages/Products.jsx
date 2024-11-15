c973c9dbba865062508962d6c262975bc3bcdfc3
prince1096 <princeraj10121996@gmail.com>
2024-11-15 20:26:37 +0530

    uploadProduct

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import UploadProduct from "../components/UploadProduct";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div>
+      <div className="bg-white py-2 px-4 flex justify-between items-center">
+        <h2 className="font-bold text-lg">All Products</h2>
+        <button className="border-2 bg-gray-400 border-black hover:bg-black hover:text-white  transition-all py-2 px-4 rounded-full">
+          Add Product
+        </button>
+      </div>
+
+      {/* Upload product component */}
+      <UploadProduct />
+    </div>
+  );
 };
 
 export default Products;
