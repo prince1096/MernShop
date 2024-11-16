a6f035a2387aa50848320752714072c3dfb4c393
prince1096 <princeraj10121996@gmail.com>
2024-11-17 00:58:48 +0530

    helperfunction

--- frontend/src/helpers/addImage.js
+++ frontend/src/helpers/addImage.js
@@ -1,5 +1,13 @@
-const url = `https://api.cloudinary.com/v1_1/dejiizvkx/image`;
+import axios from "axios";
 
-const addImage = async (image) => {};
+const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME_CLOUDINARY}/image/upload`;
+
+const addImage = async (image) => {
+  const formData = new FormData();
+  formData.append("file", image);
+  formData.append("upload_preset");
+
+  const responseData = await axios.post(url);
+};
 
 export default addImage;
