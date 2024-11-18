23005d98f33145adfda3d9322d8eb420d2d0b4ad
prince1096 <princeraj10121996@gmail.com>
2024-11-18 23:09:09 +0530

    clouder

--- frontend/src/helpers/addImage.js
+++ frontend/src/helpers/addImage.js
@@ -5,9 +5,10 @@ const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME_
 const addImage = async (image) => {
   const formData = new FormData();
   formData.append("file", image);
-  formData.append("upload_preset");
+  formData.append("upload_preset", "mern_shop");
 
   const responseData = await axios.post(url);
+  console.log(responseData);
 };
 
 export default addImage;
