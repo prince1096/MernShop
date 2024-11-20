2b2dc86d22968456dbe6f2ac7e25c2e44ccd927f
prince1096 <princeraj10121996@gmail.com>
2024-11-20 21:26:16 +0530

    getAllproductController

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -123,9 +123,31 @@ const uploadProductController = async (req, res) => {
   }
 };
 
+// Get Product
+
+const getAllProductController = async (req, res) => {
+  try {
+    const allProduct = await Product.find({});
+
+    res.json({
+      message: "All Product",
+      success: true,
+      error: false,
+      data: allProduct,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
+
 module.exports = {
   allUsersController,
   singleUserController,
   updateUserController,
   uploadProductController,
+  getAllProductController,
 };
