d1270823ee9c09db7d012293334e359855e79523
prince1096 <princeraj10121996@gmail.com>
2024-12-03 20:57:30 +0530

    productbackend

--- backend/controller/product.Controller.js
+++ backend/controller/product.Controller.js
@@ -4,7 +4,22 @@ const getCategoryProductController = async (req, res) => {
   try {
     const productCategory = await Product.distinct("category");
 
-    // console.log();
+    const productByCategory = [];
+
+    for (const category of productCategory) {
+      const product = await Product.findOne({ category });
+
+      if (product) {
+        productByCategory.push(product);
+      }
+    }
+
+    res.status(200).json({
+      message: "Product by Category",
+      data: productByCategory,
+      success: true,
+      error: false,
+    });
   } catch (error) {
     res.status(404).json({
       message: error.message || error,
