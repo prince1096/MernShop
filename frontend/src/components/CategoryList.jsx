57b6014b3bbfcca09f7febd9745474def424ed86
prince1096 <princeraj10121996@gmail.com>
2024-12-08 21:59:41 +0530

    displaycategory

--- frontend/src/components/CategoryList.jsx
+++ frontend/src/components/CategoryList.jsx
@@ -21,17 +21,20 @@ const CategoryList = () => {
 
   return (
     <div className="container mx-auto p-4 ">
-      <div className="flex items-center">
+      <div className="flex items-center gap-4 justify-between overflow-scroll scrollbar-none">
         {categoryProduct.map((product, index) => {
           return (
             <div className="p-2">
-              <div className="w-20 h-20 rounded-full overflow-hidden">
+              <div className=" w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden p-3 bg-white flex items-center justify-center">
                 <img
                   src={product?.productImage[0]}
                   alt={product?.category}
                   className="h-full object-fill"
                 />
               </div>
+              <p className="text-center text-sm md:text-base">
+                {product?.category}
+              </p>
             </div>
           );
         })}
