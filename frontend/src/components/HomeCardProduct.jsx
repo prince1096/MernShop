73f57a476bd74436a42db441fc74ca4a3d5afea7
prince1096 <princeraj10121996@gmail.com>
2024-12-17 21:03:35 +0530

    fetchCategorywiseProduct

--- frontend/src/components/HomeCardProduct.jsx
+++ frontend/src/components/HomeCardProduct.jsx
@@ -1,21 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import fetchCategoryWiseProduct from "../helpers/fetchCategoryWiseProduct";
 
 const HomeCardProduct = ({ category, heading }) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const loadingList = new Array(13).fill(null);
 
+  const fetchData = async () => {
+    setLoading(true);
+    const categoryProduct = await fetchCategoryWiseProduct(category);
+    setLoading(false);
+
+    setData(categoryProduct?.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="container mx-auto px-4 my-6">
-      HomeCardProduct
       <h2 className="text-2xl font-semibold py-2 ">{heading}</h2>
-      <div className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] h-36 bg-white rounded-sm shadow flex">
-        <div className="bg-slate-200 h-full p-2 min-w-[120px] md:min-w-[145px]">
-          <img src="" alt="ok" />
-        </div>
+      {data?.map((product, index) => {
+        return (
+          <div className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] h-36 bg-white rounded-sm shadow flex">
+            <div className="bg-slate-200 h-full p-2 min-w-[120px] md:min-w-[145px]">
+              <img src="" alt="ok" />
+            </div>
 
-        <div></div>
-      </div>
+            <div></div>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- frontend/src/helpers/fetchCategoryWiseProduct.js
+++ frontend/src/helpers/fetchCategoryWiseProduct.js
@@ -2,5 +2,23 @@ import axios from "axios";
 import SummaryApi from "../common";
 
 const fetchCategoryWiseProduct = async (category) => {
-  const response = await axios(SummaryApi.getProductByCategory.url);
+  try {
+    const response = await axios.post(
+      SummaryApi.getProductByCategory.url,
+      { category }, // Request body
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const responseData = response.data; // Axios automatically parses JSON responses
+    return responseData; // Return the data for further use
+  } catch (error) {
+    console.error("Error fetching category-wise product:", error);
+    throw error; // Propagate the error for handling
+  }
 };
+
+export default fetchCategoryWiseProduct;
