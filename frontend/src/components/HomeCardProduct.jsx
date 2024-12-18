cd8041fef00c7c423c44a4523156170f8ab9bd42
prince1096 <princeraj10121996@gmail.com>
2024-12-18 22:41:49 +0530

    displayproduct

--- frontend/src/components/HomeCardProduct.jsx
+++ frontend/src/components/HomeCardProduct.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import fetchCategoryWiseProduct from "../helpers/fetchCategoryWiseProduct";
+import displayINRCurrrency from "../helpers/displayCurrency";
+import { FaAngleRight } from "react-icons/fa6";
+import { FaAngleLeft } from "react-icons/fa6";
 
 const HomeCardProduct = ({ category, heading }) => {
   const [data, setData] = useState([]);
@@ -20,9 +23,22 @@ const HomeCardProduct = ({ category, heading }) => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 my-6">
+    <div className="container mx-auto px-4 my-6 relative">
       <h2 className="text-2xl font-semibold py-2 ">{heading}</h2>
       <div className="flex items-center gap-4 md:gap-6 overflow-scroll scrollbar-none">
+        <button
+          // onClick={prevImage}
+          className="bg-white shadow-md rounded-full p-1 ml-4 absolute left-0 z-99"
+        >
+          <FaAngleLeft />
+        </button>
+        <button
+          // onClick={prevImage}
+          className="bg-white shadow-md rounded-full p-1 mr-4 absolute right-0 z-99"
+        >
+          <FaAngleRight />
+        </button>
+
         {data?.map((product, index) => {
           return (
             <div className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] h-36 bg-white rounded-sm shadow flex">
@@ -33,8 +49,25 @@ const HomeCardProduct = ({ category, heading }) => {
                   className="object-scale-down h-full"
                 />
               </div>
-
-              <div></div>
+              {/* <div className="flex justify-center items-center"> */}
+              <div className="px-4 py-2">
+                <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black">
+                  {product?.productName}
+                </h2>
+                <p className="capitalize text-slate-500">{product?.category}</p>
+                <div className="flex gap-2 justify-between text-sm ">
+                  <p className="font-medium">
+                    {displayINRCurrrency(product?.sellingPrice)}
+                  </p>
+                  <p className="text-slate-500 line-through">
+                    {displayINRCurrrency(product?.price)}
+                  </p>
+                </div>
+                <button className="bg-black text-white hover:bg-gray-600 px-3 py-0.5 mt-7 rounded-full">
+                  Add to Cart
+                </button>
+              </div>
+              {/* </div> */}
             </div>
           );
         })}
