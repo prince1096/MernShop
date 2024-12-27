74ba9a6dd36d631f8c7a724d8c98c459bd59c7d5
prince1096 <princeraj10121996@gmail.com>
2024-12-27 22:23:52 +0530

    productdetails

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   const params = useParams();
+
+  const [data, setData] = useState({
+    productName: "",
+    brandName: "",
+    category: "",
+    productImage: [],
+    description: "",
+    price: 0,
+    sellingPirce: 0,
+  });
 
   console.log(params);
   return <div>ProductDetails</div>;
